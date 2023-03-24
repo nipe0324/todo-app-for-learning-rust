9f26815f2c77f15229657c225b12a1d2ed76261c
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-03-24 23:26:21 +0900

    add POST /users

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use serde::Serialize;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Serialize, Deserialize};
 use sqlx::{Row, SqlitePool};
 
 #[derive(Serialize)]
@@ -7,6 +7,12 @@ struct Todo {
     id: i32,
     title: String,
     completed: bool
+}
+
+#[derive(Deserialize)]
+pub struct NewTodo {
+    title: String,
+    description: Option<String>,
 }
 
 #[get("/todos")]
@@ -35,6 +41,40 @@ async fn get_todos(pool: web::Data<SqlitePool>) -> impl Responder {
     }
 }
 
+#[post("/todos")]
+async fn post_todos(
+    pool: web::Data<SqlitePool>,
+    new_todo: web::Json<NewTodo>,
+) -> impl Responder {
+    let result = sqlx::query(
+        r#"
+        INSERT INTO todos (title, description, completed, created_at, updated_at)
+        VALUES ($1, $2, false, datetime('now'), datetime('now'))
+        RETURNING id, title, description, completed, created_at, updated_at;
+        "#,
+    )
+    .bind(&new_todo.title)
+    .bind(new_todo.description.as_deref())
+    .fetch_one(pool.get_ref())
+    .await;
+
+    match result {
+        Ok(row) => {
+            let todo = Todo {
+                id: row.get("id"),
+                title: row.get("title"),
+                completed: row.get("completed"),
+            };
+
+            HttpResponse::Created().json(todo)
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     // connect to SQLite DB
@@ -45,6 +85,7 @@ async fn main() -> Result<(), std::io::Error> {
         App::new()
             .app_data(web::Data::new(pool.clone()))
             .service(get_todos)
+            .service(post_todos)
     })
     .bind("127.0.0.1:8080")?
     .run()
