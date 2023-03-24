fe00205dd48952032a6daf601f0a736341ca101b
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-03-24 23:20:04 +0900

    use todo.db in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,32 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use actix_web::web::Data;
 use serde::Serialize;
 use sqlx::{Row, SqlitePool};
 
 #[derive(Serialize)]
-struct User {
+struct Todo {
     id: i32,
-    name: String,
+    title: String,
+    completed: bool
 }
 
-#[get("/users")]
-async fn get_users(pool: web::Data<SqlitePool>) -> impl Responder {
-    let result = sqlx::query("SELECT id, name FROM users")
+#[get("/todos")]
+async fn get_todos(pool: web::Data<SqlitePool>) -> impl Responder {
+    let result = sqlx::query("SELECT id, title, completed FROM todos")
         .fetch_all(pool.get_ref())
         .await;
 
     match result {
         Ok(rows) => {
-            let users: Vec<User> = rows
+            let todos: Vec<Todo> = rows
                 .into_iter()
-                .map(|row| User {
+                .map(|row| Todo {
                     id: row.get("id"),
-                    name: row.get("name"),
+                    title: row.get("title"),
+                    completed: row.get("completed"),
                 })
                 .collect();
 
-            HttpResponse::Ok().json(users)
+            HttpResponse::Ok().json(todos)
         }
         Err(e) => {
             eprintln!("Error: {}", e);
@@ -36,35 +37,14 @@ async fn get_users(pool: web::Data<SqlitePool>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
-    let database_url = "sqlite::memory:";
+    // connect to SQLite DB
+    let database_url = "todo.db";
     let pool = SqlitePool::connect(&database_url).await.unwrap();
-
-    // テーブルを作成
-    sqlx::query(
-        r#"
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL
-        );
-    "#,
-    )
-    .execute(&pool)
-    .await
-    .unwrap();
-
-    sqlx::query("INSERT INTO users (name) VALUES ('Alice')")
-        .execute(&pool)
-        .await
-        .unwrap();
-    sqlx::query("INSERT INTO users (name) VALUES ('Bob')")
-        .execute(&pool)
-        .await
-        .unwrap();
 
     HttpServer::new(move || {
         App::new()
-            .app_data(Data::new(pool.clone()))
-            .service(get_users)
+            .app_data(web::Data::new(pool.clone()))
+            .service(get_todos)
     })
     .bind("127.0.0.1:8080")?
     .run()
