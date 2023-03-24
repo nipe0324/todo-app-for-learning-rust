5b0623117c31b54846830a3599667c4ba28dddbc
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-03-24 22:50:49 +0900

    add GET /users

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,45 @@
-use sqlx::sqlite::SqlitePool;
-use sqlx::Row;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::web::Data;
+use serde::Serialize;
+use sqlx::{Row, SqlitePool};
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // connect to database
+#[derive(Serialize)]
+struct User {
+    id: i32,
+    name: String,
+}
+
+#[get("/users")]
+async fn get_users(pool: web::Data<SqlitePool>) -> impl Responder {
+    let result = sqlx::query("SELECT id, name FROM users")
+        .fetch_all(pool.get_ref())
+        .await;
+
+    match result {
+        Ok(rows) => {
+            let users: Vec<User> = rows
+                .into_iter()
+                .map(|row| User {
+                    id: row.get("id"),
+                    name: row.get("name"),
+                })
+                .collect();
+
+            HttpResponse::Ok().json(users)
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
     let database_url = "sqlite::memory:";
-    let conn = SqlitePool::connect(&database_url).await?;
+    let pool = SqlitePool::connect(&database_url).await.unwrap();
 
-    // create table
+    // テーブルを作成
     sqlx::query(
         r#"
         CREATE TABLE IF NOT EXISTS users (
@@ -16,28 +48,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         );
     "#,
     )
-    .execute(&conn)
-    .await?;
+    .execute(&pool)
+    .await
+    .unwrap();
 
-    // add two records
     sqlx::query("INSERT INTO users (name) VALUES ('Alice')")
-        .execute(&conn)
-        .await?;
+        .execute(&pool)
+        .await
+        .unwrap();
     sqlx::query("INSERT INTO users (name) VALUES ('Bob')")
-        .execute(&conn)
-        .await?;
+        .execute(&pool)
+        .await
+        .unwrap();
 
-    // select users
-    let rows = sqlx::query("SELECT id, name FROM users")
-        .fetch_all(&conn)
-        .await?;
-
-    // print result
-    for row in rows {
-        let id: i32 = row.get("id");
-        let name: String = row.get("name");
-        println!("User {}: {}", id, name);
-    }
-
-    Ok(())
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(pool.clone()))
+            .service(get_users)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
